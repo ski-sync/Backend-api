6470079ab123e7d0dc4c346e5c99e5d1dfed3c1f
Enzo Parriel <enzo.parriel@gmail.com>
2023-11-28 16:03:47 +0100

    feat: :rocket: Add RoleModule and RoleGuard to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseService } from './database/database.service';
 import { AuthModule } from './auth/auth.module';
+import { RoleModule } from './role/role.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RoleGuard } from './role/role.guard';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, RoleModule],
   controllers: [AppController],
-  providers: [AppService, DatabaseService],
+  providers: [AppService, DatabaseService, { provide: APP_GUARD, useClass: RoleGuard }],
 })
 export class AppModule {}
