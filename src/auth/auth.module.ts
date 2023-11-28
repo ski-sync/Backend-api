a45c3f20c2f07895d8b8416d56d31322661a9386
Louis.S <louis.sasse@protonmail.com>
2023-11-28 12:26:15 +0100

    feature: Add AuthGuard and JwtConfiguration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import jwtConfiguration from 'src/config/jwt.configuration';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      load: [jwtConfiguration],
+    }),
+    JwtModule.registerAsync({
+      useFactory: async (config: ConfigType<typeof jwtConfiguration>) => ({
+        secret: config.secret,
+        signOptions: { expiresIn: config.expiresIn },
+      }),
+      imports: [ConfigModule.forFeature(jwtConfiguration)],
+      inject: [jwtConfiguration.KEY],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
