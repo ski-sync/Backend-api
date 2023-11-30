f7b378d35edabd1a6636bb7a4609b00db006979c
Louis.S <louis.sasse@protonmail.com>
2023-11-30 15:43:08 +0100

    feature: Add user-related endpoints and decorators

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
 
 export class UserDto {
   @ApiProperty()
   uuid: string;
   @ApiProperty()
-  username: string;
+  name: string;
   @ApiProperty()
   email: string;
+  @ApiProperty()
+  @Exclude()
+  password: string;
   @ApiProperty()
   createdAt: Date;
   @ApiProperty()
