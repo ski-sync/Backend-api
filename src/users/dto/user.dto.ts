6b81c87479d5f4910ea1ff5e989e26cc450af499
Louis.S <louis.sasse@protonmail.com>
2023-12-17 14:40:43 +0100

    Update(5): UserDto with API property descriptions

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -2,19 +2,66 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
 
 export class UserDto {
-  @ApiProperty()
+  @ApiProperty({
+    type: 'string',
+    description: 'The UUID of the User',
+    format: 'uuid',
+    uniqueItems: true,
+  })
   uuid: string;
-  @ApiProperty()
+
+  @ApiProperty({
+    type: 'string',
+    description: 'The name of the User',
+    minLength: 1,
+    maxLength: 255,
+  })
   name: string;
-  @ApiProperty()
+
+  @ApiProperty({
+    type: 'string',
+    description: 'The email of the User',
+    minLength: 1,
+    maxLength: 255,
+  })
   email: string;
-  @ApiProperty()
+
+  @ApiProperty({
+    type: 'string',
+    description: 'The password of the User',
+    minLength: 1,
+    maxLength: 255,
+  })
   @Exclude()
   password: string;
-  @ApiProperty()
+
+  @ApiProperty({
+    type: 'string',
+    description: 'The role of the User',
+    enum: ['admin', 'user'],
+  })
+  role: string;
+
+  @ApiProperty({
+    type: 'string',
+    description: 'The createdAt of the User',
+    format: 'date-time',
+    required: false,
+  })
   createdAt: Date;
-  @ApiProperty()
+
+  @ApiProperty({
+    type: 'string',
+    description: 'The updatedAt of the User',
+    format: 'date-time',
+    required: false,
+  })
   updatedAt: Date;
-  @ApiProperty()
+  @ApiProperty({
+    type: 'string',
+    description: 'The deletedAt of the User',
+    format: 'date-time',
+    required: false,
+  })
   deletedAt: Date;
 }
