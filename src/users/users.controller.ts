0e20b732230f292b1ed83047548e76867ac75362
Louis.S <louis.sasse@protonmail.com>
2023-12-17 14:13:11 +0100

    feature: Add delete user functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './users.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { RoleGuard } from 'src/roles/roles.guard';
@@ -8,11 +8,19 @@ import { Roles } from 'src/roles/roles.decorator';
 import { plainToInstance } from 'class-transformer';
 import { SearchUserDto } from './dto/search-user.dto';
 import { AuthUser } from 'src/auth/auth.decorator';
+import { DeleteUserDto } from './dto/delete-user.dto';
 @UseGuards(AuthGuard, RoleGuard)
 @Controller('users')
+/**
+ * Controller class for managing user-related operations.
+ */
 export class UsersController {
   constructor(private userService: UserService) {}
 
+  /**
+   * Get all users.
+   * @returns A promise that resolves to an array of UserDto objects.
+   */
   @ApiCreatedResponse({
     description: 'Get all users',
     type: [UserDto],
@@ -24,6 +32,11 @@ export class UsersController {
     return users.map(user => plainToInstance(UserDto, user));
   }
 
+  /**
+   * Search users based on the provided search criteria.
+   * @param searchUserDto - The search criteria.
+   * @returns A promise that resolves to an array of UserDto objects.
+   */
   @ApiCreatedResponse({
     description: 'Search users',
     type: [UserDto],
@@ -40,14 +53,36 @@ export class UsersController {
     return users.map(user => plainToInstance(UserDto, user));
   }
 
+  /**
+   * Get the currently authenticated user.
+   * @param user - The authenticated user.
+   * @returns A promise that resolves to a UserDto object.
+   */
   @ApiCreatedResponse({
     description: 'Get user',
     type: UserDto,
   })
   @Get('me')
-  @Roles(['admin', 'user'])
   async getMe(@AuthUser() user: any): Promise<UserDto> {
     const findUser = await this.userService.findOne({ uuid: user.sub });
+    return plainToInstance(UserDto, findUser);
+  }
+
+  /**
+   * Delete user.
+   * @param uuid - The user uuid.
+   * @returns A promise that resolves to a UserDto object.
+   */
+
+  @ApiCreatedResponse({
+    description: 'Delete user',
+    type: UserDto,
+  })
+  @Delete('delete')
+  @Roles(['admin'])
+  async deleteUser(@Body() uuid: DeleteUserDto): Promise<UserDto> {
+    const findUser = await this.userService.findOne({ uuid: uuid.uuid });
+    await this.userService.deleteUser(uuid);
     return plainToInstance(UserDto, findUser);
   }
 }
