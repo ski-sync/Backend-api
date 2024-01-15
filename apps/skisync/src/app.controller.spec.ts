a7fc8381ab83b507d031f729abc7091726c6ba5f
LSASSE <louis.sasse@protonmail.com>
2024-01-15 11:21:14 +0100

    fix: delete test

--- apps/skisync/src/app.controller.spec.ts
+++ apps/skisync/src/app.controller.spec.ts
@@ -1,22 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfiguration from '@lib/config/jwt.configuration';
+import { jwtValidationSchema } from '@lib/config/jwt.validation';
+import { RoleModule } from './roles/roles.module';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          load: [jwtConfiguration],
+          validationSchema: jwtValidationSchema,
+        }),
+        JwtModule,
+        RoleModule,
+      ],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appService = module.get<AppService>(AppService);
+    appController = module.get<AppController>(AppController);
   });
 
-  describe('root', () => {
+  describe('getHello', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      jest.spyOn(appService, 'getHello').mockImplementation(() => 'Hello World!');
+
+      const result = appController.getHello();
+
+      expect(result).toBe('Hello World!');
     });
   });
+
+  // Add more test cases as needed for different scenarios, such as testing the guards, etc.
 });
