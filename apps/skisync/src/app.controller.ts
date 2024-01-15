e8909a8d421c0333c89eb597af9ad32977089a4c
LSASSE <louis.sasse@protonmail.com>
2024-01-15 11:37:39 +0100

    fix: Refactor app.controller.ts and app.controller.spec.ts

--- apps/skisync/src/app.controller.ts
+++ apps/skisync/src/app.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
