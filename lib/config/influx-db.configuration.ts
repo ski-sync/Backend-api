7f8855888dca26a078d76488e42ac43fc7e02b64
code-ex0 <louis.sasse@protonmail.com>
2024-02-21 10:01:52 +0100

    fix: Update InfluxDB URL in configuration

--- lib/config/influx-db.configuration.ts
+++ lib/config/influx-db.configuration.ts
@@ -1,5 +1,5 @@
 export const influxDBConfig = {
-  url: 'http://localhost:8086',
+  url: 'http://influxdb:8086',
   token: 'P-hFBMpt6Xr1s1-D-USdPJXYpXuBx8esT_ONnR0K902rEL-HPzJ5JFQ-eyIBja8LvKLFs7LnN_TYLjRAKPHiaQ==',
   org: 'skisync',
   orgID: 'ba0f6a183415f9d5',
